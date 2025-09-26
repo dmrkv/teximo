50c88d21ec09bf1ad3b5909c547fb996d5fb7fdf
Dmitrii Markov <phdmarkov@gmail.com>
2025-09-26 18:18:59 +0200

    Fix menu item to show Ctrl+Shift instead of Alt+Shift

--- Sources/Teximo/SimpleAppDelegate.swift
+++ Sources/Teximo/SimpleAppDelegate.swift
@@ -54,7 +54,7 @@ class SimpleAppDelegate: NSObject, NSApplicationDelegate {
         statusMenu.addItem(switchItem)
         
         let transliterationItem = NSMenuItem(title: "Transliterate Text", action: #selector(testTransliteration), keyEquivalent: "⇧")
-        transliterationItem.keyEquivalentModifierMask = [.control]
+        transliterationItem.keyEquivalentModifierMask = [.control, .shift]
         transliterationItem.target = self
         statusMenu.addItem(transliterationItem)
         statusMenu.addItem(NSMenuItem.separator())
